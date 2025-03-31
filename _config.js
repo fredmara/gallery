f96cffedbdb91ee0201231edfb676c88f08bdf57
fredmara <53741743+fredmara@users.noreply.github.com>
2025-03-31 09:51:50 +0300

    Update _config.js

--- _config.js
+++ _config.js
@@ -2,8 +2,6 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://moringa:moringa1@gallery.wc344.mongodb.net/gallery?retryWrites=true&w=majority',
-    development: 'mongodb+srv://moringa:moringa1@gallery.wc344.mongodb.net/gallery?retryWrites=true&w=majority',
-    test: 'mongodb+srv://moringa:moringa1@gallery.wc344.mongodb.net/gallery?retryWrites=true&w=majority',
+        Database: 'mongodb+srv://moringa:moringa1@gallery.wsidhbf.mongodb.net/gallery?retryWrites=true&w=majority',
 }
 module.exports = config;
