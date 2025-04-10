01e309258e9bb611810f24f9d1217a73ecf2b490
fredmara <fredmara2016@gmail.com>
2025-04-10 16:39:56 +0300

    config commit

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://fowino:moringa1@moringa1.wi7j66y.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://fowino:moringa1@moringa1.wi7j66y.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://fowino:moringa1@moringa1.wi7j66y.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
